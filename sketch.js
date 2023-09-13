8d145413e15d92e659bbe3dd54ebb7df5552f5fa
YIHAN WANG <yw7451@nyu.edu>
2023-09-13 15:23:59 -0400

    added two ellipsis and one square

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
+    createCanvas(600, 600);
+    background(204,229,255);
 }
 
 function draw() {
+    fill(255, 204, 229);
+    stroke(255);
+    strokeWeight(3);
+    ellipse(300, 300, 300, 300);
+    fill(255, 255, 204);
+    stroke(255,153,153);
+    ellipse(400, 300, 200, 200);
+    rect(300,300,200,200);
 }
